ffae90c61c0f55e42786b0e9f189fdb67a560e0e
EnzoRochaBrito <enzo1brich@gmail.com>
2025-03-20 16:57:20 -0300

    melhorando contato

--- js/form.js
+++ js/form.js
@@ -14,21 +14,28 @@ class contato {
 
 function Post(form) {
 
-  let data = new contato(form.elements.namedItem("nome").value,
-            form.elements.namedItem("sobrenome").value, 
-            form.elements.namedItem("email").value, 
-            form.elements.namedItem("cpf").value, 
-            form.elements.namedItem("telefone").value, 
-            form.elements.namedItem("contato").value);
-  
+    let data = new contato(form.elements.namedItem("nome").value,
+                form.elements.namedItem("sobrenome").value, 
+                form.elements.namedItem("email").value, 
+                form.elements.namedItem("cpf").value, 
+                form.elements.namedItem("telefone").value, 
+                form.elements.namedItem("contato").value);
+
+    if (data.contato == 1){
+        alert("Selecione um meio de contato");
+        return;
+    } 
+
+    Enviar(data);
+    
 }
 
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+function Enviar(pessoa) {
+    
+    var nome = pessoa.nome;
+    
+    if (nome != "") {
+        alert('Obrigado sr(a) ' + nome + ' os seus dados foram encaminhados com sucesso');
     }
 
 }
