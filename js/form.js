70983703737a594aed9955e64efe2a7b86b5cadc
EnzoRochaBrito <enzo1brich@gmail.com>
2025-03-26 16:58:56 -0300

    adicionando validador

--- js/form.js
+++ js/form.js
@@ -23,7 +23,13 @@ function Post(form) {
     if (data.contato == 1){
         alert("Selecione um meio de contato");
         return;
-    } 
+    }
+
+    if (VerificarCpf(data.cpf)){
+        alert('cpf valido');
+    } else {
+        alert('cpf invalido')
+    }
 
     Enviar(data);
     
@@ -37,4 +43,68 @@ function Enviar(pessoa) {
         document.location = 'index.html';
         alert('Obrigado sr(a) ' + nome + ' os seus dados foram encaminhados com sucesso');
     }
+}
+
+function VerificarCpf(cpf, vezes = 0, digits = []){
+
+
+    console.log('inicio f')
+    console.log(cpf)
+    console.log(vezes)
+    console.log(digits)
+    
+    let soma   = 0;
+    
+    if (cpf.length != 11){
+        return false;
+    }
+    
+    // adiciona os dígitos do cpf em um array
+    for (let i = 0; i < cpf.length-2; i++){
+        try {
+            digits.push(parseInt(cpf[i]))
+        } catch (error) {
+            return false;
+        }
+    }
+    console.log('meio f')
+    console.log(digits)
+    
+    // multiplica cada número da lista e faz a soma
+    digits.forEach((element, id) => {
+        soma += (element * (10+vezes-id));
+    });
+    
+    // resto da divisão
+    let resto = soma % 11;
+    
+    // compara com o dígito verificador
+    if (resto < 2){
+        console.log(resto)
+        console.log(cpf[9+vezes] + "resto")
+        console.log(digits)
+        if (parseInt(cpf[9+vezes]) != 0){
+            return false;
+        } else{
+            digits.push(0);
+        }
+    } else {
+        console.log(resto)
+        console.log(cpf[9+vezes])
+        console.log((11 - resto ) + ' resto')
+        if (parseInt(cpf[9+vezes] != (11-resto))){
+            return false;
+        }else{
+            digits.push(11-resto);
+        }
+    }
+    
+    if (vezes = 1){
+        console.log('fim f')
+        console.log(digits)
+        return true;
+    }
+    
+    VerificarCpf(cpf, 1, digits)
+    
 }
